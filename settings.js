199c8a7c11a8144d053c01b188a9fbb278ed351e
devshadow14 <nd172237@gmail.com>
2026-08-02 13:30:22 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,35 +1,35 @@
 const settings = {
-  packname: 'DEV SHADOW MD V1.0',
-  author: 'DEV SHADOW TECH',
-  botName: "DEV SHADOW MD V1.0",
-  botOwner: 'DEV SHADOW TECH',
+  packname: '𝐌𝐈𝐂𝐇𝐀𝐄𝐋 𝐒𝐂𝐎𝐅𝐈𝐄𝐋𝐃 𝐌𝐃 𝐕1.0',
+  author: '𝐃𝐄𝐕 𝐌𝐈𝐂𝐇𝐀𝐄𝐋.𝐒𝐂𝐎𝐅𝐈𝐄𝐋𝐃',
+  botName: "𝐌𝐈𝐂𝐇𝐄𝐀𝐋 𝐒𝐂𝐎𝐅𝐈𝐄𝐋𝐃 𝐌𝐃 𝐕1.0",
+  botOwner: '𝐃𝐄𝐕 𝐌𝐈𝐂𝐇𝐀𝐄𝐋 𝐒𝐂𝐎𝐅𝐈𝐄𝐋𝐃',
   phoneNumber: "221711192303", // Non utilisé pour le pairing (désormais géré par Telegram /pair), gardé pour compatibilité avec main.js
   ownerNumber: '221711192303', // Numéro du propriétaire principal du bot (admin des commandes .owner, .sudo, etc.)
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20,
   storeWriteInterval: 10000,
-  description: "DEV SHADOW MD — Bot WhatsApp en français, rapide, multi-device, piloté via Telegram.",
+  description: "MICHAEL SCOFIELD MD — Bot WhatsApp en français, rapide, multi-device, piloté via Telegram.",
   version: "1.0.0",
-  channelLink: "https://whatsapp.com/channel/0029VbAlfhMAInPdvqEmAf2N",
+  channelLink: "https://whatsapp.com/channel/0029Vb86OeIHVvTfZBkfYx2Y",
   updateZipUrl: "https://github.com/STIVE-DEVX/ZENTROX-MD-main.zip",
-  botPreview: "https://i.postimg.cc/Dz3mbLwc/bot-image-f-Of28PVl.jpg",
+  botPreview: "https://files.catbox.moe/hu6ia4.jpg",
 
   // Carte globale
   adReply: {
-    title: "DEV SHADOW MD V1.0",
-    body: "BY DEV SHADOW TECH",
-    thumbnailUrl: 'https://i.postimg.cc/c4KWPt69/chatgpt-image-3-dec-2025-17-48-55-PQCmcx-Vn.jpg',
-    sourceUrl: 'https://whatsapp.com/channel/0029VbAlfhMAInPdvqEmAf2N',
+    title: "𝐌𝐈𝐂𝐇𝐀𝐄𝐋 𝐒𝐂𝐎𝐅𝐈𝐄𝐋𝐃 𝐌𝐃 𝐕1.0",
+    body: "𝐛𝐲 𝐃𝐄𝐕 𝐌𝐈𝐂𝐇𝐀𝐄𝐋 𝐒𝐂𝐎𝐅𝐈𝐄𝐋𝐃",
+    thumbnailUrl: 'https://files.catbox.moe/hu6ia4.jpg',
+    sourceUrl: 'https://whatsapp.com/channel/0029Vb86OeIHVvTfZBkfYx2Y',
     mediaType: 1,
-    mediaUrl: 'https://whatsapp.com/channel/0029VbAlfhMAInPdvqEmAf2N',
+    mediaUrl: 'https://whatsapp.com/channel/0029Vb86OeIHVvTfZBkfYx2Y',
     renderLargerThumbnail: false
   },
 
   // Newsletter global (optionnel, utilisable dans toutes les commandes)
   newsletter: {
-    newsletterJid: '120363402057857053@newsletter',
-    newsletterName: 'DEV SHADOW MD V1.0',
+    newsletterJid: '0029Vb86OeIHVvTfZBkfYx2Y@newsletter',
+    newsletterName: 'MICHEAL SCOFIELD MD',
     serverMessageId: -1
   }
 };
